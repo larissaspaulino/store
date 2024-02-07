85239cc05bd02c5d5c16b9fe53ec7bcfe499566d
Larissa Paulino <larissaspaulino@gmail.com>
2024-02-07 09:34:02 -0300

    OrderStatus enum

--- src/main/java/br/com/macabea/course/entites/Order.java
+++ src/main/java/br/com/macabea/course/entites/Order.java
@@ -1,5 +1,6 @@
 package br.com.macabea.course.entites;
 
+import br.com.macabea.course.entites.enums.OrderStatus;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.persistence.*;
 
@@ -24,13 +25,26 @@ public class Order {
     @JoinColumn(name = "client_id")
     private User client;
 
+    private Integer orderStatus;
+
     public Order() {
     }
 
-    public Order(Long id, Instant moment, User client) {
+    public Order(Long id, Instant moment, User client, OrderStatus orderStatus) {
         this.id = id;
         this.moment = moment;
         this.client = client;
+        setOrderStatus(orderStatus);
+    }
+
+    public OrderStatus getOrderStatus() {
+        return OrderStatus.valueOf(orderStatus);
+    }
+
+    public void setOrderStatus(OrderStatus orderStatus) {
+        if (orderStatus != null) {
+            this.orderStatus = orderStatus.getCode();
+        }
     }
 
     public Long getId() {
