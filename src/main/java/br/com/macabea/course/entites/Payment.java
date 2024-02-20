e4736962db7dde1e6c0b5baa610dab1d6524818b
Larissa Paulino <larissaspaulino@gmail.com>
2024-02-20 09:46:57 -0300

    Subtotal & Total methods

--- src/main/java/br/com/macabea/course/entites/Payment.java
+++ src/main/java/br/com/macabea/course/entites/Payment.java
@@ -1,5 +1,6 @@
 package br.com.macabea.course.entites;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 
 import java.io.Serial;
@@ -18,6 +19,7 @@ public class Payment implements Serializable {
   private Long Id;
   private Instant moment;
 
+  @JsonIgnore
   @OneToOne
   @MapsId
   private Order order;
