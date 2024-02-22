4de3497db3d83d9f2c15acdbf106f90283fa50c3
Larissa Paulino <larissaspaulino@gmail.com>
2024-02-22 19:04:28 -0300

    Exception handling - findById

--- src/main/java/br/com/macabea/course/services/UserService.java
+++ src/main/java/br/com/macabea/course/services/UserService.java
@@ -2,6 +2,7 @@ package br.com.macabea.course.services;
 
 import br.com.macabea.course.entites.User;
 import br.com.macabea.course.repositories.UserRepository;
+import br.com.macabea.course.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
@@ -22,7 +23,7 @@ public class UserService {
     public User findById(Long id) {
         Optional<User> obj = repository.findById(id);
 
-        return obj.get();
+        return obj.orElseThrow(() -> new ResourceNotFoundException(id));
     }
 
     public User insert(User obj) {
